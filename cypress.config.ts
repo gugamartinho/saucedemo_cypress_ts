f608afab549965fae5e84c9777090a7e00b81fa3
David <gugamartinho@gmail.com>
2026-05-22 16:55:11 +0100

    .env variables

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,3 +1,6 @@
+import * as dotenv from "dotenv";
+dotenv.config(); // Load environment variables from .env file
+
 import { defineConfig } from "cypress";
 
 export default defineConfig({
@@ -11,7 +14,11 @@ export default defineConfig({
     screenshotOnRunFailure: true
   },
   e2e: {
-    baseUrl: "https://www.saucedemo.com",
+    baseUrl: process.env.BASE_URL,
+    env: {
+      VALID_USERNAME: process.env.VALID_USERNAME,
+      VALID_PASSWORD: process.env.VALID_PASSWORD
+    },
     setupNodeEvents(on, config) {
       // event listeners
     },
--- cypress/e2e/login/login.cy.ts
+++ cypress/e2e/login/login.cy.ts
@@ -9,18 +9,18 @@ describe('Login Tests', () => {
     });
 
     it('should login successfully with valid credentials', () => {
-        loginPage.login('standard_user', 'secret_sauce');
+        loginPage.login(Cypress.env('VALID_USERNAME'), Cypress.env('VALID_PASSWORD'));
         // Assert that the URL includes '/inventory.html' after successful login
         cy.url().should('include', '/inventory.html');
     });
 
     it('should display an error message with invalid password', () => {
-        loginPage.login('standard_user', 'invalid_password');
+        loginPage.login(Cypress.env('VALID_USERNAME'), 'invalid_password');
         loginPage.assertErrorMessage('Epic sadface: Username and password do not match any user in this service');
     });
 
     it('should display an error message with invalid username', () => {
-        loginPage.login('invalid_username', 'secret_sauce');
+        loginPage.login('invalid_username', Cypress.env('VALID_PASSWORD'));
         loginPage.assertErrorMessage('Epic sadface: Username and password do not match any user in this service');
     });
 
@@ -30,7 +30,7 @@ describe('Login Tests', () => {
     });
 
     it('should display an error message with locked user', () => {
-        loginPage.login('locked_out_user', 'secret_sauce');
+        loginPage.login('locked_out_user', Cypress.env('VALID_PASSWORD'));
         loginPage.assertErrorMessage('Epic sadface: Sorry, this user has been locked out.');
     });
 });
